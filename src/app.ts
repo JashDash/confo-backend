1ccaed0996bb4996e8243d56fad282de0f097a1c
Aditya Rana <official.adityarana01@gmail.com>
2021-09-07 18:19:58 +0530

    Boilerplate code

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,43 @@
-console.log("hello world");
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+import { Application, Request, Response, NextFunction } from "express";
+import HttpException from "./exceptions/HttpException";
+
+const indexRouter = require("./routes/indexRoute");
+
+const app: Application = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+
+// catch 404 and forward to error handler
+app.use((req: Request, res: Response, next: NextFunction) => {
+  next(createError(404));
+});
+
+// error handler
+app.use(
+  (err: HttpException, req: Request, res: Response, next: NextFunction) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.json(res);
+  }
+);
+
+app.listen(8000, () => {
+  console.log("Server listening");
+});
+
+module.exports = app;
