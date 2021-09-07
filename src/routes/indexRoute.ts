f51d2bf652271f45c1b84b07c56b82d103604f4a
Aditya Rana <official.adityarana01@gmail.com>
2021-09-07 18:39:04 +0530

    Resolved warnings

--- src/routes/indexRoute.ts
+++ src/routes/indexRoute.ts
@@ -1,9 +1,10 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response } from "express";
 const express = require("express");
 const router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req: Request, res: Response, next: NextFunction) {
+router.get("/", function (req: Request, res: Response) {
+  console.log(req);
   res.send("Hello world!");
 });
 
