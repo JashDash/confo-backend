3d542983c463fd9c9aa414ff60c64dfe228789b3
Aditya Rana <official.adityarana01@gmail.com>
2021-09-08 17:11:57 +0530

    Added text question model

--- src/models/textQuestionModel.ts
+++ src/models/textQuestionModel.ts
@@ -2,7 +2,26 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const textQuestionSchema = new Schema({
-  label: String,
+  label: {
+    type: String,
+    required: true,
+  },
+  inputType: {
+    type: String,
+    required: true,
+  },
+  optional: {
+    type: Boolean,
+    default: false,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  exampleInput: {
+    type: String,
+    default: "",
+  },
 });
 
 const TextQuestion = mongoose.model("TextQuestion", textQuestionSchema);
