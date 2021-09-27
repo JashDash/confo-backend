3569b012e4b62cc6912ffa72811b3e218c5e5d0f
Aditya Rana <official.adityarana01@gmail.com>
2021-09-27 23:15:46 +0530

    Added get responses route

--- src/routes/formRoute.ts
+++ src/routes/formRoute.ts
@@ -5,6 +5,7 @@ const router = express.Router();
 router.post("/metadata", controllers.postMetadata);
 router.get("/metadata/:id", controllers.getMetadata);
 router.get("/getall", controllers.getAllForms);
+router.get("/responses/:id", controllers.getResponses);
 router.post("/response", controllers.submitResponse);
 
 module.exports = router;
